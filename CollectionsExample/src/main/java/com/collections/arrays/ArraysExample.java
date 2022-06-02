3676b9c0c9fffa8c2cc34e35dd662e49e78a9ba1
Krishna <gkrrish.11@gmail.com>
2022-06-02 13:59:37 +0530

    updated

--- CollectionsExample/src/main/java/com/collections/arrays/ArraysExample.java
+++ CollectionsExample/src/main/java/com/collections/arrays/ArraysExample.java
@@ -4,4 +4,5 @@ public class ArraysExample {
 	public static void main(String[] args) {
 	}
 
-}
+}//https://www.java2novice.com/java-collections-and-util/collections/
+//https://www.studytonight.com/java-util/java-arrays-class
